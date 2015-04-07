5d5a79a108fbe6c2dbcc38f4b0973e9b4aaf8904
Raido Purga <raido@heathmont.net>
2015-04-07 12:55:15 +0300

    Reformatted client view

--- application/views/desktop/trainer/client.php
+++ application/views/desktop/trainer/client.php
@@ -1,25 +1,1 @@
-<script>
-	$(function() {
-		$('tr').click(function() {
-			document.location = '/my/clients/' + $(this).attr('client');
-		});
-	});
-</script>
-<div class="main">
-	<div class="container">
-		<div class="row single-top">
-
-			<!--	Left hand menu	-->
-
-			<div class="col-md-4 ">
-				<?$this->load->view('desktop/trainer/client_menu')?>
-				<?$this->load->view('desktop/my/my_menu')?>
-			</div>
-
-			<!--	Content	-->
-
-			kliendi andmed, treeneri märkused
-
-		</div>
-	</div>
-</div>
+<div class="main">	<div class="container">		<div class="row single-top">			<!--	Left hand menu	-->			<div class="col-md-4 ">				<?$this->load->view('desktop/trainer/client_menu')?>				<?$this->load->view('desktop/my/my_menu')?>			</div>			<!--	Content	-->			kliendi andmed, treeneri märkused		</div>	</div></div>
