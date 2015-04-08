a0d89fb8cb07c3eb6b179fb264268d8e9bbfd932
DoubleAces <raido@heathmont.net>
2015-04-08 21:53:18 +0300

    Client side mobile

---
+++ application/views/mobile/my/my_menu.php
@@ -0,0 +1,10 @@
+<ul class="blog-list">
+	<li class="no-list-style"><h4>Minu konto</h4></li>
+	<?
+	if ($this->session->activeUser->is_trainer == 1) {
+		?><li><a href="/my/clients">Kliendid</a></li><?
+	}
+	?>
+	<li><a href="/my/training">Treeningkavad</a></li>
+	<li><a href="/logout">Logout</a></li>
+</ul>
---
+++ application/views/mobile/my/plans.php
@@ -0,0 +1,44 @@
+<script>
+	$(function() {
+		$('tr').click(function() {
+			document.location = '/my/training/plan/' + $(this).attr('plan');
+		});
+	});
+</script>
+<div class="main">
+	<div class="container">
+		<div class="row single-top">
+
+			<!--	Left hand menu	-->
+
+			<div class="col-md-4 ">
+				<?$this->load->view('desktop/my/my_menu')?>
+			</div>
+
+			<!--	Content	-->
+
+			<div class="col-md-8">
+
+				<table>
+					<thead>
+						<tr>
+							<th>Treener</th>
+							<th>Treeningkava</th>
+							<th>Loodud</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?foreach($plans as $plan):?>
+							<tr plan="<?=$plan->id?>">
+								<td><?=$plan->trainer_name?></td>
+								<td><?=$plan->name?></td>
+								<td><?=formatDate($plan->added)?></td>
+							</tr>
+						<?endforeach?>
+					</tbody>
+				</table>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
+</div>
